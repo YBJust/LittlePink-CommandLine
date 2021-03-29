81348d95e63f8ca78ff4247a8b2cdf77eafbe88f
BingYang <bingyang@caixin.com>
2021-03-29 12:19:57 +0800

    init

---
+++ LittlePink-CommandLine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LittlePink-CommandLine
+//
+//  Created by Bing-Y on 2021/3/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
